2b13679e4108a196bebd133d85f1e67b4b43553b
Ian <iann909041@gmail.com>
2021-06-05 13:42:50 -0300

    HeaderAnimado

---
+++ src/pages/HomePage.js
@@ -0,0 +1,16 @@
+import styles from '../styles/HomePage.module.css'
+
+export default function HomePage(){
+  return(
+    <>
+      <header className={styles.Header}>
+        <a>INÍCIO</a>
+        <a>PORTFÓLIO</a>
+        <a>PRESET</a>
+      </header>
+      <main className={styles.Main}>
+      
+      </main>
+    </>
+  )
+}
